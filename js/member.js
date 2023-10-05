f967f397f678e385c3633282d8b41f4cd66d7dc6
派蒙 <75768445+paimengpaifjb@users.noreply.github.com>
2023-10-05 21:02:53 +0800

    Update member.js

--- js/member.js
+++ js/member.js
@@ -1,6 +1,6 @@
 var member=[
-{"phone":"No.1","name":"创新组"},
-{"phone":"No.2","name":"敬业组"},
-{"phone":"No.3","name":"专注组"},
-{"phone":"No.4","name":"精益组"},
+{"phone":"星型网络拓扑结构","name":"创新组"},
+{"phone":"环形网络拓扑结构","name":"敬业组"},
+{"phone":"分析组","name":"专注组"},
+{"phone":"总线型网络拓扑结构","name":"精益组"},
 ]
